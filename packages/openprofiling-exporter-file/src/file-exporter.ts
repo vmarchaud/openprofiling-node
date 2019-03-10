ace658f5877ea386b2d23f26805e290af6646eac
vmarchaud <contact@vmarchaud.fr>
2019-03-10 17:36:07 +0100

    feat: implement file exporter

---
+++ packages/openprofiling-exporter-file/src/file-exporter.ts
@@ -0,0 +1,40 @@
+'use strict'
+
+import { Exporter, Profile } from '../../openprofiling-core'
+import * as fs from 'fs'
+import { tmpdir } from 'os'
+import { resolve } from 'path'
+
+export class FileExporterConfig {
+  path: string
+}
+
+const defaultFileExporterConfig: FileExporterConfig = {
+  path: tmpdir()
+}
+
+export class FileExporter implements Exporter {
+
+  private config: FileExporterConfig = defaultFileExporterConfig
+
+  constructor (options?: FileExporterConfig) {
+    if (typeof options === 'object') {
+      this.config = options
+    }
+  }
+
+  onProfileStart (profile: Profile) {
+    return
+  }
+
+  onProfileEnd (profile: Profile) {
+    const filename = `${profile.kind.toLowerCase()}-${profile.startTime}-${profile.name}`
+    const targetPath = resolve(this.config.path, filename)
+    fs.writeFile(targetPath, profile.data, (err) => {
+      if (err) {
+        console.error(`Error while writing profile to disk`, err.message)
+      }
+    })
+  }
+
+}
