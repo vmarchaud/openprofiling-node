e97494d2c4592cb9817293d4814d08ac457d2134
vmarchaud <contact@vmarchaud.fr>
2019-04-22 18:14:25 +0200

    feat: implement inspector heap profiler

--- examples/express.ts
+++ examples/express.ts
@@ -1,11 +1,11 @@
 
 import { ProfilingAgent } from '../packages/openprofiling-nodejs'
 import { FileExporter } from '../packages/openprofiling-exporter-file'
-import { InspectorCPUProfiler } from '../packages/openprofiling-inspector-cpu-profiler'
+import { InspectorHeapProfiler } from '../packages/openprofiling-inspector-heap-profiler'
 import { TriggerSignal } from '../packages/openprofiling-trigger-signal'
 
 const profilingAgent = new ProfilingAgent()
-profilingAgent.register(new TriggerSignal({ signal: 'SIGUSR2' }), new InspectorCPUProfiler({}))
+profilingAgent.register(new TriggerSignal({ signal: 'SIGUSR2' }), new InspectorHeapProfiler({}))
 profilingAgent.start({ exporter: new FileExporter(), logLevel: 4 })
 
 setInterval(_ => {
---
+++ packages/openprofiling-inspector-heap-profiler/src/profiler.ts
@@ -0,0 +1,109 @@
+
+import { BaseProfiler, Trigger, TriggerState, ProfilerOptions, Profile, ProfileType, ProfileStatus } from '@openprofiling/core'
+import * as inspector from 'inspector'
+
+export class InspectorHeapProfilerOptions implements ProfilerOptions {
+  session?: inspector.Session
+  /**
+   * The less the interval is, the more the profiler is precise and the cost
+   * in term of performance is high
+   */
+  samplingInterval?: number
+}
+
+export class InspectorHeapProfiler extends BaseProfiler {
+
+  private session: inspector.Session | undefined
+  private started: boolean = false
+  private currentProfile: Profile | undefined
+
+  protected options: InspectorHeapProfilerOptions
+
+  constructor (options?: InspectorHeapProfilerOptions) {
+    super('inspector-heap', options)
+  }
+
+  init () {
+    if (typeof this.options.session === 'object') {
+      this.session = this.options.session
+      // try to connect the session if not already the case
+      try {
+        this.session.connect()
+      } catch (err) {
+        this.logger.debug('failed to connect to given session', err.message)
+      }
+    } else {
+      this.session = new inspector.Session()
+      try {
+        this.session.connect()
+      } catch (err) {
+        this.logger.error('Could not connect to inspector', err.message)
+        return
+      }
+    }
+    this.session.post('HeapProfiler.enable')
+  }
+
+  destroy () {
+    if (this.session === undefined) return
+
+    if (this.started === true) {
+      this.stopProfiling()
+    }
+    this.session.post('HeapProfiler.disable')
+  }
+
+  onTrigger (trigger: Trigger, state: TriggerState) {
+    if (this.session === undefined) {
+      throw new Error(`Session wasn't initialized`)
+    }
+    if (state === TriggerState.START && this.started === true) {
+      this.logger.info('Received start trigger but already started, ignoring')
+      return
+    }
+    if (state === TriggerState.END && this.started === false) {
+      this.logger.error('Received end trigger but wasnt started, ignoring')
+      return
+    }
+
+    if (state === TriggerState.START) {
+      this.logger.info(`Starting profiling`)
+      this.currentProfile = new Profile('toto', ProfileType.HEAP_PROFILE)
+      this.started = true
+      this.session.post('HeapProfiler.startSampling', {
+        samplingInterval: typeof this.options.samplingInterval === 'number' ?
+          this.options.samplingInterval : undefined
+      })
+      this.agent.notifyStartProfile(this.currentProfile)
+      return
+    }
+
+    if (state === TriggerState.END) {
+      this.logger.info(`Stopping profiling`)
+      this.stopProfiling()
+    }
+  }
+
+  private stopProfiling () {
+    if (this.session === undefined) {
+      throw new Error(`Session wasn't initialized`)
+    }
+    this.session.post('HeapProfiler.stopSampling', (err, params) => {
+      if (this.currentProfile === undefined) return
+      if (err) {
+        this.logger.error(`Failed to stop Heap profiler`, err.message)
+        this.currentProfile.status = ProfileStatus.FAILED
+        this.currentProfile.addAttribute('error', err.message)
+      } else {
+        const data = JSON.stringify(params.profile)
+        this.currentProfile.addProfileData(Buffer.from(data))
+        this.currentProfile.status = ProfileStatus.SUCCESS
+        this.currentProfile.addAttribute('profiler', this.name)
+      }
+
+      this.agent.notifyEndProfile(this.currentProfile)
+      this.started = false
+      this.currentProfile = undefined
+    })
+  }
+}
