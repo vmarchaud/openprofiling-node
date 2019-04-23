65d94b22acb544303811347e4e537bd6ff0d14cb
vmarchaud <contact@vmarchaud.fr>
2019-04-23 17:07:22 +0200

    chore: update example

--- examples/express.ts
+++ examples/express.ts
@@ -2,10 +2,14 @@
 import { ProfilingAgent } from '../packages/openprofiling-nodejs'
 import { FileExporter } from '../packages/openprofiling-exporter-file'
 import { InspectorHeapProfiler } from '../packages/openprofiling-inspector-heap-profiler'
-import { TriggerSignal } from '../packages/openprofiling-trigger-signal'
+import { InspectorCPUProfiler } from '../packages/openprofiling-inspector-cpu-profiler'
+import { SignalTrigger } from '../packages/openprofiling-trigger-signal'
+import * as inspector from 'inspector'
 
 const profilingAgent = new ProfilingAgent()
-profilingAgent.register(new TriggerSignal({ signal: 'SIGUSR2' }), new InspectorHeapProfiler({}))
+const session = new inspector.Session()
+profilingAgent.register(new SignalTrigger({ signal: 'SIGUSR2' }), new InspectorHeapProfiler({ session }))
+profilingAgent.register(new SignalTrigger({ signal: 'SIGUSR1' }), new InspectorCPUProfiler({ session }))
 profilingAgent.start({ exporter: new FileExporter(), logLevel: 4 })
 
 setInterval(_ => {
